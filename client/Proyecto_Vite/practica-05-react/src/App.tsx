1713346909e77c942e0982189ec70d308a97efd9
Cesar Castillo <cuh512410168@cuh.mx>
2026-01-26 10:52:13 -0600

    Primera App

--- client/Proyecto_Vite/practica-05-react/src/App.tsx
+++ client/Proyecto_Vite/practica-05-react/src/App.tsx
@@ -1,35 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState<string[]>([])
+
+  const addTask = () => {
+    if (task.trim() === '') return
+    setTasks([...tasks, task])
+    setTask('')
+  }
+
+  const deleteTask = (index: number) => {
+    setTasks(tasks.filter((_, i) => i !== index))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-container">
+      <h1>Actvidades a Realizar</h1>
+
+      <div className="form">
+        <input
+          type="text"
+          placeholder=" Escribe los pendientes"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTask}>➕ Agregar</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="list">
+        {tasks.map((t, i) => (
+          <div className="list-item" key={i}>
+            <span>📌 {t}</span>
+            <button onClick={() => deleteTask(i)}>✖</button>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
